219ef6d8e1128bff5e2dbe4e5403c19a75032767
Angela Mashlan <Angela.Mashlan@Safeco.com>
2017-03-13 11:18:51 -0700

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,11 @@
+console.log("Up and running!");
+
+//set var for cards
+var cardOne = "queen";
+var cardTwo = "queen";
+var cardThree = "king";
+var cardFour = "king";
+
+//flip cards
+console.log("User flipped " + cardOne);
+console.log("User flipped " + cardTwo);
