285f260080dd7d32890c084d8d23d9c6e6b43396
Angela Mashlan <Angela.Mashlan@Safeco.com>
2017-03-13 14:11:17 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,11 +1,28 @@
 console.log("Up and running!");
 
 //set var for cards
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+var cards = ["queen", "queen", "king", "king"]
 
-//flip cards
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo);
+//variable to hold cards in play
+var cardsInPlay = [];
+
+//variable to hold the first flipped card
+var cardOne = cards[0];
+
+cardsInPlay.push(cardOne);
+
+console.log("User flipped " + cardsInPlay[cardsInPlay.length - 1]);
+
+var cardTwo = cards[1];
+
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped " + cardsInPlay[cardsInPlay.length - 1]);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!")
+	} else {
+		alert("Sorry, try again.")
+		}
+}
