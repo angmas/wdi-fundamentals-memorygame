0d071aa003e272bfb5480e89b9bb3010553a21dd
Angela Mashlan <Angela.Mashlan@Safeco.com>
2017-03-13 19:47:38 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,28 @@
 console.log("Up and running!");
 
-//set var for cards
-var cards = ["queen", "queen", "king", "king"]
+//set objects for cards
+var cards = [
+	{
+		rank: "queen",
+		suit: "hearts",
+		cardImage: "images/queen-of-hearts.png"
+	},
+	{
+		rank: "queen",
+		suit: "diamonds",
+		cardImage: "images/queen-of-diamonds.png"
+	},
+	{
+		rank: "king",
+		suit: "hearts",
+		cardImage: "images/king-of-hearts.png"
+	},
+	{
+		rank: "king",
+		suit: "diamonds",
+		cardImage: "images/king-of-diamonds.png"
+	}
+]
 
 //variable to hold cards in play
 var cardsInPlay = [];
@@ -21,9 +42,10 @@ var checkForMatch = function() {
 var flipCard = function (cardId) {
 	//variable to hold the first flipped card
 
-	console.log("User flipped " + cards[cardId]);
-
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+	cardsInPlay.push(cards[cardId].rank);
 	checkForMatch();
 
 	
